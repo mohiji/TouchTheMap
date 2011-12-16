f9daad5797bdb2480f34306d1119157d30c11927
Jonathan Fischer <jonathan@mohiji.org>
2011-12-15 20:46:12 -0800

    Initial Commit

---
+++ TouchTheMap/TouchTheMapAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TouchTheMapAppDelegate.h
+//  TouchTheMap
+//
+//  Created by Jonathan Fischer on 12/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TouchTheMapAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
